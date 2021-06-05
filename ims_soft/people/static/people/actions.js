6a1d6571d8e9c5844248eea9b824401b99e607ca
fouadsan <f.benayad95@gmail.com>
2021-06-05 20:37:20 +0100

    cleaned people/actions.js

--- ims_soft/people/static/people/actions.js
+++ ims_soft/people/static/people/actions.js
@@ -24,50 +24,31 @@ var globalVariableDel;
 // })
 
 
-<<<<<<< HEAD
 $(document).on('click', '#update-btn', function () {
     var upId = $(this).attr('data-item');
-=======
-$(document).on('click','#update-btn',function(){
-    var upId=$(this).attr('data-item');
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
     globalVariableUp = upId
     $.ajax({
         type: 'GET',
         url: url + upId,
-<<<<<<< HEAD
         success: salaryInput ? function (response) {
             console.log(salaryInput)
 
-=======
-        success: salaryInput ? function(response){
-            console.log(salaryInput)
-    
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
             const data = response.data
             nameInput.value = data.name
             phoneInput.value = data.phone
             addressInput.value = data.address
             salaryInput.value = data.salary
             downPayInput.value = data.down_payments
-<<<<<<< HEAD
 
         } : function (response) {
             console.log(response.data)
 
-=======
-    
-        } : function(response){
-            console.log(response.data)
-    
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
             const data = response.data
             nameInput.value = data.name
             emailInput.value = data.email
             phoneInput.value = data.phone
             faxInput.value = data.fax
             addressInput.value = data.address
-<<<<<<< HEAD
 
         },
         error: function (error) {
@@ -78,23 +59,10 @@ $(document).on('click','#update-btn',function(){
 })
 
 updateForm.addEventListener('submit', e => {
-=======
-    
-        },
-        error: function(error){
-            console.log(error)
-        }
-    })
-    
-})
-
-updateForm.addEventListener('submit', e=>{
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
     e.preventDefault()
 
     var localUpId = globalVariableUp
 
-<<<<<<< HEAD
     const id = document.getElementById('id-' + localUpId)
     console.log(id.textContent)
     const name = document.getElementById('name-' + localUpId)
@@ -104,17 +72,6 @@ updateForm.addEventListener('submit', e=>{
     const address = document.getElementById('address-' + localUpId)
     const salary = document.getElementById('salary-' + localUpId)
     const down_payments = document.getElementById('down_payments-' + localUpId)
-=======
-    const id = document.getElementById('id-'+localUpId)
-    console.log(id.textContent)
-    const name = document.getElementById('name-'+localUpId)
-    const email = document.getElementById('email-'+localUpId)
-    const phone = document.getElementById('phone-'+localUpId)
-    const fax = document.getElementById('fax-'+localUpId)
-    const address = document.getElementById('address-'+localUpId)
-    const salary = document.getElementById('salary-'+localUpId)
-    const down_payments = document.getElementById('down_payments-'+localUpId)
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
 
     $.ajax({
         type: 'POST',
@@ -134,20 +91,12 @@ updateForm.addEventListener('submit', e=>{
             'fax': faxInput.value,
             'address': addressInput.value
         },
-<<<<<<< HEAD
         success: function (response) {
-=======
-        success: function(response){
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
             $('#updateModal').modal('hide')
             handleAlerts('success', 'Object has been updated')
 
             name.textContent = response.name
-<<<<<<< HEAD
             if (response.hasOwnProperty('email')) {
-=======
-            if(response.hasOwnProperty('email')) {
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
                 response.email != "" ? email.textContent = response.email : email.textContent = "N/A"
                 response.fax != "" ? fax.textContent = response.fax : fax.textContent = "N/A"
             } else if (response.hasOwnProperty('salary')) {
@@ -155,24 +104,16 @@ updateForm.addEventListener('submit', e=>{
                 response.down_payments != "" ? down_payments.textContent = response.down_payments : down_payments.textContent = "N/A"
             }
             response.phone != "" ? phone.textContent = response.phone : phone.textContent = "N/A"
-<<<<<<< HEAD
             response.address != "" ? address.textContent = response.address : address.textContent = "N/A"
 
         },
         error: function (error) {
-=======
-            response.address != "" ? address.textContent = response.address : address.textContent = "N/A" 
-
-        },
-        error: function(error){
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
             console.log(error)
         }
     })
 
 })
 
-<<<<<<< HEAD
 $(document).on('click', '#delete-btn', function () {
     var delId = $(this).attr('data-item');
     const delName = $(this).attr('data-item-name');
@@ -184,28 +125,11 @@ $(document).on('click', '#delete-btn', function () {
 })
 
 deleteForm.addEventListener('submit', e => {
-=======
-$(document).on('click','#delete-btn',function(){
-    var delId=$(this).attr('data-item');
-    const delName=$(this).attr('data-item-name');
-    globalVariableDel = delId
-
-    var delMsg = document.getElementById("delMsg");
-    delMsg.insertAdjacentHTML('afterend', `<b>${delName}</b>`) ;
-    
-})
-
-deleteForm.addEventListener('submit', e=>{
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
     e.preventDefault()
 
     var localDelId = globalVariableDel
 
-<<<<<<< HEAD
     const id = document.getElementById('id-' + localDelId)
-=======
-    const id = document.getElementById('id-'+localDelId)
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
 
 
     $.ajax({
@@ -214,7 +138,6 @@ deleteForm.addEventListener('submit', e=>{
         data: {
             'csrfmiddlewaretoken': csrf[0].value,
         },
-<<<<<<< HEAD
         success: function (response) {
             id.parentNode.remove()
             searchInput.value = ""
@@ -224,15 +147,6 @@ deleteForm.addEventListener('submit', e=>{
             getData()
         },
         error: function (error) {
-=======
-        success: function(response){
-            id.parentNode.remove()
-            $('#deleteModal').modal('hide')
-            handleAlerts('success', response.msg)
-            localStorage.setItem('name', nameInput.value)
-        },
-        error: function(error){
->>>>>>> f8d56b506ebe52bcb849e5ab5e4e29c9e8dd6750
             console.log(error)
         }
     })
